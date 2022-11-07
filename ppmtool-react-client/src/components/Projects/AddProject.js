ca2be6230e291c991184376d0900b3283506f164
shangchenhsieh <seankh4444@gmail.com>
2022-11-07 13:28:52 -0800

    Nov 7

--- ppmtool-react-client/src/components/Projects/AddProject.js
+++ ppmtool-react-client/src/components/Projects/AddProject.js
@@ -1,10 +1,70 @@
 import React, { Component } from 'react'
 
 class AddProject extends Component {
+  constructor() {
+    super();
+    this.state={
+      projectIdentifier: "",
+      projectName: "",
+      description: "",
+      startDate: "",
+      endDate: ""
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Add Project Form</h1>
+      {
+        //check name attribute in input field
+        //create constructor
+        //set state
+        //set value on input fields
+        //create onChange function
+        //set onChange on each input field
+        //bind on constructor
+        //check state change in react extension
+      }
+        <div className="project">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 mauto">
+                <h5 className="display-4 text-center">Create project form</h5>
+                <hr />
+                <form>
+
+                  <div className="form-group">
+                    <input type="text" className="form-control form-control-lg" placeHolder="Project Name" name="projectName"/>
+                  </div>
+
+                  <div className="form-group">
+                    <input type="text" className="form-control form-control-lg" placeHolder="Unique Project ID" name="projectIdentifier"/>
+                  </div>
+
+                  <div className="form-group">
+                    <textarea className="form-control form-control-lg" placeholder="Project Description" name="description"></textarea>
+                  </div>
+
+                  <h6>Start Date</h6>
+                  <div className="form-group">
+                    <input type="date" className="form-control form-control-lg" name="start_date"></input>
+                  </div>
+
+                  <h6>Estimate End Date</h6>
+                  <div className="form-group">
+                    <input type="date" className="form-control form-control-lg" name="end_date"></input>
+                  </div>
+
+                  <input type="submit" classname="btn btn-primary btn-block mt-4"></input>
+
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
       </div>
     )
   }
