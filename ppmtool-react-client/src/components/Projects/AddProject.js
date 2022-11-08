7300620b028f39b284420c2b5de38d4243b2c200
shangchenhsieh <seankh4444@gmail.com>
2022-11-07 22:20:56 -0800

    wired up addProject

--- ppmtool-react-client/src/components/Projects/AddProject.js
+++ ppmtool-react-client/src/components/Projects/AddProject.js
@@ -10,7 +10,27 @@ class AddProject extends Component {
       startDate: "",
       endDate: ""
     }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
+  onChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newProject = {
+      projectIdentifier: this.state.projectIdentifier,
+      projectName: this.state.projectName,
+      description: this.state.description,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate 
+    }
+    console.log(newProject);
+  }
+
   render() {
     return (
       <div>
@@ -30,28 +50,33 @@ class AddProject extends Component {
               <div className="col-md-8 mauto">
                 <h5 className="display-4 text-center">Create project form</h5>
                 <hr />
-                <form>
+                <form onSubmit={this.onSubmit}>
 
                   <div className="form-group">
-                    <input type="text" className="form-control form-control-lg" placeHolder="Project Name" name="projectName"/>
+                    <input type="text" className="form-control form-control-lg" 
+                    placeHolder="Project Name" name="projectName" value={this.state.projectName} onChange={this.onChange}/>
                   </div>
 
                   <div className="form-group">
-                    <input type="text" className="form-control form-control-lg" placeHolder="Unique Project ID" name="projectIdentifier"/>
+                    <input type="text" className="form-control form-control-lg" 
+                    placeHolder="Unique Project ID" name="projectIdentifier" value={this.state.projectIdentifier} onChange={this.onChange} />
                   </div>
 
                   <div className="form-group">
-                    <textarea className="form-control form-control-lg" placeholder="Project Description" name="description"></textarea>
+                    <textarea className="form-control form-control-lg" 
+                    placeHolder="Project Description" name="description" value={this.state.description} onChange={this.onChange}></textarea>
                   </div>
 
                   <h6>Start Date</h6>
                   <div className="form-group">
-                    <input type="date" className="form-control form-control-lg" name="start_date"></input>
+                    <input type="date" className="form-control form-control-lg" 
+                    name="startDate" value={this.state.startDate} onChange={this.onChange}></input>
                   </div>
 
                   <h6>Estimate End Date</h6>
                   <div className="form-group">
-                    <input type="date" className="form-control form-control-lg" name="end_date"></input>
+                    <input type="date" className="form-control form-control-lg" 
+                    name="endDate" value={this.state.endDate} onChange={this.onChange}></input>
                   </div>
 
                   <input type="submit" classname="btn btn-primary btn-block mt-4"></input>
