2b291bb58408caf20d2ee9adfd4411aaaaa0ca38
shangchenhsieh <seankh4444@gmail.com>
2022-11-14 09:58:57 -0800

    a

--- ppmtool-react-client/src/components/Projects/AddProject.js
+++ ppmtool-react-client/src/components/Projects/AddProject.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { createProject } from "../../actions/projectActions";
-import classnames from "classname"
+import classnames from "classnames"
 
 class AddProject extends Component {
   constructor() {
