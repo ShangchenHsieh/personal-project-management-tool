5c9231bce6dda8f54f70c318ad7057dbb9684458
shangchenhsieh <seankh4444@gmail.com>
2022-11-16 21:02:27 -0800

    nov 16

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,20 +1,20 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post("http://localhost:8080/api/project", project);
+    const res = await axios.post("/api/project", project);
     history.push("/dashboard");
   } catch (err) {
     dispatch({
       type: GET_ERRORS,
-      payload: err.response.data
+      payload: {}
     });
   }
 };
 
 export const getProjects = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080/api/project/all");
+  const res = await axios.get("/api/project/all");
   dispatch({
     type: GET_PROJECTS,
     payload: res.data
@@ -24,13 +24,22 @@ export const getProjects = () => async dispatch => {
 export const getProject = (id, history) => async dispatch => {
 
   try {
-    const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+    const res = await axios.get(`/api/project/${id}`);
     dispatch({
       type: GET_PROJECT,
       payload: res.data
     });
   } catch(error) {
-    history.push("dashboard")
+    history.push("/dashboard")
   }
 
 };
+
+export const deleteProject = id => async dispatch => {
+  await axios.delete(`/api/project/${id}`)
+  dispatch({
+    type: DELETE_PROJECT,
+    payload: state.projects.filter(project => project.projectIdentifier !== action.payload) 
+    //dynamically get rid of deleted projects w/o refreshing 
+  })
+}
--- ppmtool-react-client/src/components/Projects/ProjectItem.js
+++ ppmtool-react-client/src/components/Projects/ProjectItem.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types"
+import {connect} from "react-redux"
+import { deleteProject } from "../../actions/projectActions"
+
 
 class ProjectItem extends Component {
   render() {
@@ -41,4 +45,9 @@ class ProjectItem extends Component {
   }
 }
 
-export default ProjectItem;
+
+ProjectItem.propTypes = {
+  deleteProject: PropTypes.func.isRequired,
+  
+}
+export default connect(null, {deleteProject})(ProjectItem);
