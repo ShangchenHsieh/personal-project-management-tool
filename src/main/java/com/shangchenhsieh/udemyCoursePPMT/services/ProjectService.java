2c39d953f80a8eb143703c6d6641c076f633ddc9
shangchenhsieh <seankh4444@gmail.com>
2022-12-31 07:40:58 -0800

    dec 31

--- src/main/java/com/shangchenhsieh/udemyCoursePPMT/services/ProjectService.java
+++ src/main/java/com/shangchenhsieh/udemyCoursePPMT/services/ProjectService.java
@@ -8,7 +8,6 @@ import com.shangchenhsieh.udemyCoursePPMT.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
 public class ProjectService {
 
     @Autowired
