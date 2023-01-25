18af545a4415e274818ea2880b8b4ccf51e834f0
shangchenhsieh <seankh4444@gmail.com>
2023-01-25 06:45:52 -0800

    Jan 25

--- ppmtool-react-client/src/components/ProjectBoard/Backlog.js
+++ ppmtool-react-client/src/components/ProjectBoard/Backlog.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react'
-import ProjectTask from './ProjectTasks/ProjectTask';
+import React, { Component } from "react";
+import ProjectTask from "./ProjectTasks/ProjectTask";
 
 class Backlog extends Component {
   render() {
     const { project_tasks_prop } = this.props;
-    
 
     const tasks = project_tasks_prop.map(project_task => (
       <ProjectTask key={project_task.id} project_task={project_task} />
     ));
 
-    let todoItems = []
-    let inProgressItems = []
-    let doneItems = []
+    let todoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
 
-    for(let i =0; i < tasks.length; i++) { 
-      console.log(tasks[i])
-      if(tasks[i].props.project_task.status == "TO_DO") {
+    for (let i = 0; i < tasks.length; i++) {
+      console.log(tasks[i]);
+
+      if (tasks[i].props.project_task.status === "TO_DO") {
         todoItems.push(tasks[i]);
       }
-      else if(tasks[i].props.project_task.status == "IN_PROGRESS") {
+
+      if (tasks[i].props.project_task.status === "IN_PROGRESS") {
         inProgressItems.push(tasks[i]);
       }
-      else {
+
+      if (tasks[i].props.project_task.status === "DONE") {
         doneItems.push(tasks[i]);
       }
-
     }
 
     return (
@@ -33,15 +34,16 @@ class Backlog extends Component {
         <div className="row">
           <div className="col-md-4">
             <div className="card text-center mb-2">
-              <div className="card-header bg-primary text-white">
+              <div className="card-header bg-secondary text-white">
                 <h3>TO DO</h3>
               </div>
             </div>
             {todoItems}
+            
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
-              <div className="card-header bg-warning text-white">
+              <div className="card-header bg-primary text-white">
                 <h3>In Progress</h3>
               </div>
             </div>
