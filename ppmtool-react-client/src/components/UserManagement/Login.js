d8d748b0feba8f75373a763327049eb78a607c76
shangchenhsieh <seankh4444@gmail.com>
2023-02-17 23:23:48 -0800

    Feb 17 react login 7 register

---
+++ ppmtool-react-client/src/components/UserManagement/Login.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+  render() {
+    return (
+        <div className="login">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8 m-auto">
+                    <h1 className="display-4 text-center">Log In</h1>
+                    <form action="dashboard.html">
+                        <div className="form-group">
+                            <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <input type="submit" className="btn btn-primary btn-lg mt-4 " />
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+  }
+}
+
+export default Login;
